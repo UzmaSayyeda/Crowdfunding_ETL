70fd7cff80f15f12cdf46f9697eb51ca5fbe883f
sroethlisberger85 <stephanieroethlisberger@Stephanies-MBP.attlocal.net>
2023-09-21 14:51:20 -0500

    created and added SQL files

---
+++ Crowdfunding_Database/Data_Analysis.sql
@@ -0,0 +1,9 @@
+---Verify that each table has the correct data
+
+select * from campaign;
+
+select * from category;
+
+select * from contacts;
+
+select * from subcategory;
--- Crowdfunding_Database/crowdfunding_db_schema.sql
+++ Crowdfunding_Database/crowdfunding_db_schema.sql
@@ -2,25 +2,7 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "campaign" (
-    "cf_id" int   NOT NULL,
-    "contact_id" int   NOT NULL,
-    "company_name" varchar   NOT NULL,
-    "description" varchar   NOT NULL,
-    "goal" float   NOT NULL,
-    "pledged" float   NOT NULL,
-    "outcome" varchar   NOT NULL,
-    "backers_count" int   NOT NULL,
-    "country" varchar   NOT NULL,
-    "currency" varchar   NOT NULL,
-    "launch_date" date   NOT NULL,
-    "end_date" date   NOT NULL,
-    "category_id" int   NOT NULL,
-    "subcategory_id" int   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
-);
+
 
 CREATE TABLE "contacts" (
     "contact_id" int   NOT NULL,
@@ -45,6 +27,26 @@ CREATE TABLE "subcategory" (
     "subcategory" varchar   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar   NOT NULL,
+    "description" varchar   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar   NOT NULL,
+    "currency" varchar   NOT NULL,
+    "launch_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" int   NOT NULL,
+    "subcategory_id" int   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
      )
 );
 
